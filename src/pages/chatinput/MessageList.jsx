96f21c8b7fdd7c537c5662bf4817161b03098ddb
usmansaleem90 <98312186+usmansaleem90@users.noreply.github.com>
2024-09-18 23:29:46 -0700

    Update MessageList.jsx

--- src/pages/chatinput/MessageList.jsx
+++ src/pages/chatinput/MessageList.jsx
@@ -33,7 +33,12 @@ const MessageList = ({ messages, loading }) => {
             <CSSTransition key={index} timeout={500} classNames="fade">
               <ListItem sx={{ borderBottom: "1px solid #424242" }}>
                 <ListItemText
-                  primary={message?.text}
+{/* primary={message?.text} */}
+                 primary={
+                    <span
+                      dangerouslySetInnerHTML={{ __html: message?.text }}
+                    />
+                  }
                   secondary={message?.sender === "bot" ? "Chatbot" : "You"}
                   primaryTypographyProps={{
                     style: {
